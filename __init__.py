d1af0928de27a976135f3f9c3adc4795bfcb89c3
pancho <pancho@localhost.localdomain>
2023-03-05 19:35:20 -0300

    nuke_util: update

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@ from .nuke_util import panels
 
 
 def setup():
-    panels.init(stock_manager.stock_manager.stock_manager_widget,
+    panels.init('stock_manager.stock_manager.stock_manager.stock_manager_widget',
                 'Stock Manager')
