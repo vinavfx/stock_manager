0d213624a13ee2fbfdbde6643e6766c6c9d8b905
pancho <pancho@localhost.localdomain>
2023-03-25 21:05:07 -0300

    message when not python 3

--- __init__.py
+++ __init__.py
@@ -1,7 +1,16 @@
-from . import src as stock_manager
-from .nuke_util import panels
+import nuke
+from sys import version_info
+
+if version_info.major == 3:
+    from . import src as stock_manager
+    from .nuke_util import panels
 
 
 def setup():
+    if not version_info.major == 3:
+        nuke.message(
+            '"Stock Manager" only works on a higher version of nuke with Python 3 !')
+        return
+
     panels.init('stock_manager.stock_manager.stock_manager.stock_manager_widget',
                 'Stock Manager')
