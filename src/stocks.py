8a0212eeb93372f67426a5b1f2c4b6cd05277e93
Francisco <fcocc77@gmail.com>
2024-11-23 18:31:13 -0300

    stocks: uft8

--- src/stocks.py
+++ src/stocks.py
@@ -4,8 +4,10 @@
 # WEBSITE -------> https://vinavfx.com
 # -----------------------------------------------------------
 import os
+import re
 import sys
-from ..python_util.util import jread
+import json
+from ..python_util.util import fread
 from ..env import INDEXING_DIR
 
 stocks_data = {}
@@ -13,7 +15,7 @@ stocks_data = {}
 
 def load_data():
     global stocks_data
-    stocks_data = jread(os.path.join(INDEXING_DIR, 'stocks.json'))
+    stocks_data = json.loads(fread(os.path.join(INDEXING_DIR, 'stocks.json')))
 
     if sys.version_info[0] < 3:
         stocks_data = convert_to_utf8(stocks_data)
@@ -25,12 +27,20 @@ def get_stocks():
 
 def convert_to_utf8(data):
     if isinstance(data, dict):
-        return {convert_to_utf8(key): convert_to_utf8(value) for key, value in data.items()}
+        return {convert_to_utf8(key): convert_to_utf8(value) for key, value in data.iteritems()}
+
     elif isinstance(data, list):
         return [convert_to_utf8(element) for element in data]
+
+    elif isinstance(data, unicode):
+        return clean_non_ascii(data).encode('utf-8')
+
     elif isinstance(data, str):
-        return data.encode('utf-8') if sys.version_info[0] < 3 else data
-    elif sys.version_info[0] < 3 and isinstance(data, unicode):
-        return data.encode('utf-8')
+        return clean_non_ascii(data)
+
     else:
         return data
+
+
+def clean_non_ascii(s):
+    return re.sub(r'[^\x00-\x7F]', '', s)
