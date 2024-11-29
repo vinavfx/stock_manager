fbec96321a0c01cd55051c9fc91e771f9fdfe572
Francisco Contreras <fcocc77@gmail.com>
2024-11-29 15:35:56 -0300

    env: update

--- env.py
+++ env.py
@@ -1,9 +1,9 @@
 import os
 
-INDEXING_DIR = '/home/pancho/Desktop/indexing'
+INDEXING_DIR = '<path_to_indexing_folder'
 
 ROOT_STOCKS_DIRS = [
-    '/home/pancho/Desktop/stocks'
+    '<path_to_stocks_folder>'
 ]
 
 FFMPEG = '/usr/bin/ffmpeg'
@@ -11,6 +11,9 @@ FFPROBE = '/usr/bin/ffprobe'
 
 STOCKS_DIRS = [os.path.join(root, d) for root in ROOT_STOCKS_DIRS if os.path.exists(
     root) for d in os.listdir(root) if os.path.isdir(os.path.join(root, d))]
+
+STOCKS_DIRS = [
+    p for p in STOCKS_DIRS if not 'indexing' in os.path.basename(p).lower()]
 
 INDEXED_DIR = os.path.join(INDEXING_DIR, 'indexed')
 THUMBNAILS_DIR = os.path.join(INDEXING_DIR, 'thumbnails')
